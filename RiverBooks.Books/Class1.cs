c7cfdf6884af737ddbde303fe21572f8efa0d9dc
Árthur Tolfo Pinheiro <arthur.pinheiro@elsys.com.br>
2025-05-24 17:20:15 -0300

    Add project files.

---
+++ RiverBooks.Books/Class1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RiverBooks.Books;
+
+internal interface IBookService
+{
+    IEnumerable<BookDto> ListBooks();
+}
+
+public record BookDto(Guid Id, string Title, string Author);
+
+internal class BookService : IBookService
+{
+    private readonly List<BookDto> _books =
+    [
+        new BookDto(Guid.NewGuid(), "The Great Gatsby", "F. Scott Fitzgerald"),
+        new BookDto(Guid.NewGuid(), "To Kill a Mockingbird", "Harper Lee"),
+        new BookDto(Guid.NewGuid(), "1984", "George Orwell")
+    ];
+    public IEnumerable<BookDto> ListBooks()
+    {
+        return _books;
+    }
+}
+
+public static class BookEndpoints
+{
+    public static void MapBookEndpoints(this WebApplication app)
+    {
+        app.MapGet("/books", (IBookService bookService) =>
+        {
+            return bookService.ListBooks();
+        });
+    }
+}
+
+public static class BookServiceExtensions
+{
+    public static IServiceCollection AddBookServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IBookService, BookService>();
+
+        return services;
+    }
+}
